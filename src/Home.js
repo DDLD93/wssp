6e7fecaaf9d5e8565c24147a82ede39b9c739c64
DDLD93 <umar.jere@email.com>
2021-08-12 21:31:32 +0100

    styling css

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import {Link} from 'react-router-dom'
+import {Link} from 'react-router-dom';
+import logoOne from './asset/sokoto.jpeg'; // with import
+import logoTwo from './asset/waffroad-1.jpeg'; // with import
 
 export default function Home() {
 
@@ -12,15 +14,19 @@ export default function Home() {
             <Link to="/explore"><button id="three">Explore Kaduna</button></Link>
             
         </div>
+        <hr style={{marginTop:'20px'}} />
+        <hr style={{width: '200px'}} />
         <div className="part-two">
             <div className="services">
-                <img width="100%" height="300" src="" alt="" />
+                <img width="100%" height="300" src={logoOne} alt="" />
                 <h1>Our Services</h1>
+                <hr />
                 <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias eum eligendi praesentium architecto atque sapiente aperiam rerum facere id doloremque, accusamus, dolores necessitatibus tempora suscipit numquam maiores laboriosam odit mollitia, ullam consequatur perspiciatis. Eius cupiditate obcaecati delectus, vel excepturi assumenda, nostrum ut quam iusto, dignissimos maiores quis quae totam accusantium!</p>
             </div>
-            <div className="safety">
-            <img width="100%" height="300" src="" alt="" />
+            <div className="services">
+            <img width="100%" height="300" src={logoTwo} alt="" />
             <h1>Focused on safety, wherever, whenever</h1>
+            <hr />
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui molestiae a voluptatum animi similique odit voluptates. Vero, nihil ad excepturi rerum corrupti ducimus libero distinctio nostrum rem voluptate. Doloribus inventore magnam quis explicabo nemo doloremque. Error eveniet qui, explicabo culpa ipsum soluta omnis nobis odit inventore itaque. Dolores, sed harum?</p>
             </div>
           <footer>
@@ -39,7 +45,7 @@ export default function Home() {
                   <h3>Get in touch with us</h3>
                   <hr style={{backgroundColor:"white"}} />
                   <div className="social-links">
-                      
+
                   </div>
 
               </div>
